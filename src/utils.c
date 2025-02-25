d2f37930f28eee373945a24595fe32f911270096
paulasopena <paulasopenacoello@gmail.com>
2025-02-25 19:05:21 +0100

    feat: encryption working

---
+++ src/utils.c
@@ -0,0 +1,26 @@
+void block_split(){
+
+}
+void key_addition_layer(){
+    
+
+}
+void byte_substitution_layer(){
+   
+
+}
+void diffusion_layer(){
+
+}
+void shift_rows(){
+
+}
+void mix_columns(){
+
+}
+void aes_encrypt(){
+
+}
+void hex_to_bytes(){
+    
+}
